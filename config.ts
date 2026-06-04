9a54bb4f09f3175dba060cfe4392f9f24867fbe8
DevCaveCore <wgreen@cavecoredynamics.org>
2026-06-03 23:25:56 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,6 +2,5 @@ export const API_KEYS = {
     FLIGHTAWARE: "ydaGQPxfpA94dNZ05GRNur8vzCofygmn",
     OWM: "20372103888ca0c31fa1bb6a11925209",
     GOOGLE_MAPS: "AIzaSyAm5lbG7x6bdpXyXBzjYM7z9HxwOOnx3yw",
-    GEMINI: "AIzaSyCLvy4N4ci3ZPUJcKLuW91F-A_3s_OeT5g"
     GEMINI: import.meta.env.VITE_GEMINI_API_KEY || "AIzaSyCLvy4N4ci3ZPUJcKLuW91F-A_3s_OeT5g"
 };
